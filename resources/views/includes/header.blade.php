459948ee20ac4509e1e5904d9803d2d5e0aff1c6
biabkaahfa <biabkaahfa@gmail.com>
2024-11-12 10:55:05 +0100

    init

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,99 @@
+<div class="container-fluid position-relative p-0">
+    <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+        {{-- <a href="" class="navbar-brand p-0">
+            <h1 class="text-primary"><i class="fas fa-search-dollar me-3"></i>Stocker</h1>
+            <!-- <img src="img/logo.png" alt="Logo"> -->
+        </a> --}}
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="fa fa-bars"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="/welcome" class="nav-item nav-link active">Accueil</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link" data-bs-toggle="dropdown">
+                        <span class="dropdown-toggle">Services</span>
+                    </a>
+                    <div class="dropdown-menu m-0">
+
+                                <a href="#" class="dropdown-item"><i></i> Simulation Thermique</a>
+                                <a href="#" class="dropdown-item"><i ></i> Simulation Mécanique</a>
+                                <a href="#" class="dropdown-item"><i></i> Couplage Thermomécanique</a>
+                                <a href="#" class="dropdown-item"><i ></i>Conseil et Formation</a>
+
+                    </div>
+                </div>
+                <a href="/realisation" class="nav-item nav-link">Réalisations</a>
+                <a href="/realisation" class="nav-item nav-link">A propos</a>
+                <a href="/realisation" class="nav-item nav-link">Contact</a>
+            </div>
+
+        </div>
+    </nav>
+
+    <!-- Carousel Start -->
+    <div class="header-carousel owl-carousel">
+        <div class="header-carousel-item">
+            <img src="img/carousel-1.jpg" class="img-fluid w-100" alt="Image">
+            <div class="carousel-caption">
+                <div class="container">
+                    <div class="row gy-0 gx-5">
+                        <div class="col-lg-0 col-xl-5"></div>
+                        <div class="col-xl-7 animated fadeInLeft">
+                            <div class="text-sm-center text-md-end">
+                                {{-- <h4 class="text-primary text-uppercase fw-bold mb-4">Welcome To Stocker</h4>
+                                <h1 class="display-4 text-uppercase text-white mb-4">Invest your money with higher return</h1>
+                                <p class="mb-5 fs-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...
+                                </p>
+                                <div class="d-flex justify-content-center justify-content-md-end flex-shrink-0 mb-4">
+                                    <a class="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2" href="#"><i class="fas fa-play-circle me-2"></i> Watch Video</a>
+                                    <a class="btn btn-primary rounded-pill py-3 px-4 px-md-5 ms-2" href="#">Learn More</a>
+                                </div> --}}
+                                <div class="d-flex align-items-center justify-content-center justify-content-md-end">
+                                    <h2 class="text-white me-2">Follow Us:</h2>
+                                    <div class="d-flex justify-content-end ms-2">
+                                        <a class="btn btn-md-square btn-light rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                                        <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-twitter"></i></a>
+                                        <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-instagram"></i></a>
+                                        <a class="btn btn-md-square btn-light rounded-circle ms-2" href=""><i class="fab fa-linkedin-in"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-carousel-item">
+            <img src="img/carousel-2.jpg" class="img-fluid w-100" alt="Image">
+            <div class="carousel-caption">
+                <div class="container">
+                    <div class="row g-5">
+                        <div class="col-12 animated fadeInUp">
+                            <div class="text-center">
+                                {{-- <h4 class="text-primary text-uppercase fw-bold mb-4">Welcome To Stocker</h4>
+                                <h1 class="display-4 text-uppercase text-white mb-4">Invest your money with higher return</h1>
+                                <p class="mb-5 fs-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...
+                                </p>
+                                <div class="d-flex justify-content-center flex-shrink-0 mb-4">
+                                    <a class="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2" href="#"><i class="fas fa-play-circle me-2"></i> Watch Video</a>
+                                    <a class="btn btn-primary rounded-pill py-3 px-4 px-md-5 ms-2" href="#">Learn More</a>
+                                </div> --}}
+                                <div class="d-flex align-items-center justify-content-center">
+                                    <h2 class="text-white me-2">Follow Us:</h2>
+                                    <div class="d-flex justify-content-end ms-2">
+                                        <a class="btn btn-md-square btn-light rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                                        <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-twitter"></i></a>
+                                        <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-instagram"></i></a>
+                                        <a class="btn btn-md-square btn-light rounded-circle ms-2" href=""><i class="fab fa-linkedin-in"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Carousel End -->
+</div>
